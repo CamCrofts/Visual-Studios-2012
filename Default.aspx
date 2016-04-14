0fa9dd22a6c2735dcb7c3c68c603f7556e63a839
CamCrofts <cameron.crofts@gmail.com>
2016-04-15 11:06:00 +1200

    Added files via upload

---
+++ Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>Customer Spend</p>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+ <br />
+        <asp:Button ID="Button1" runat="server" Text="Calculate" OnClick="Button1_Click" />
+
+        <asp:CheckBox ID="FlyBuys" runat="server" Text="Do you have flybuys?" />
+
+        <p>Discount Recived</p>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       //         var spend;
+       // var discount;
+
+       // spend = "101";
+
+       //TextBox1 = int
+       //    Button1 
+
+       //    if TextBox1 = > $100
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        double spend = double.Parse(TextBox1.Text);
+
+        if (spend > 100 && FlyBuys.Checked == true)
+        {
+            TextBox2.Text = (spend * 0.75).ToString();
+        }
+
+        else if (spend > 100 && FlyBuys.Checked == false)
+        {
+            TextBox2.Text = (spend * 0.85).ToString();
+        }
+
+        else if (spend < 100 || spend == 100)
+        {
+            TextBox2.Text = (spend * 1).ToString();
+        }
+        
+
+    }
+}
